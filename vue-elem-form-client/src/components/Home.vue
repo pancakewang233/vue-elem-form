5a03b2bde8f4c15fae49b1441f570dad649ac71d
Pank <834430775@qq.com>
2022-04-14 13:16:03 +0800

    展示接口数据

--- vue-elem-form-client/src/components/Home.vue
+++ vue-elem-form-client/src/components/Home.vue
@@ -3,11 +3,14 @@ import { ref } from "vue";
 import api from "../api/api";
 
 const msg = ref("Hello World!");
+const name = ref("");
 api.user().then((res) => {
+  name.value = res.data[0].nickname;
   console.log("fucking", res);
 });
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  {{ name }}
 </template>
